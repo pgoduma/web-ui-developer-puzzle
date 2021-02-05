45c4adc8442ecea10e0803cd6aa2d6360196d5ce
Pradeep Chandra Goduma <pgoduma@pkrmsezdc.com>
2021-02-05 18:28:31 +0530

    Updated e2e test

--- apps/okreads-e2e/src/specs/reading-list.spec.ts
+++ apps/okreads-e2e/src/specs/reading-list.spec.ts
@@ -18,8 +18,28 @@ describe('When: I use the reading list feature', () => {
     );
   });
   it('Then: Able to remove book and undo it', async () => {
-     await element(by.id('book1')).click();
-     await browser.sleep(2000);
-     await element(by.cssContainingText('.mat-button-wrapper', 'Undo')).click();
+    await browser.get('/');
+    await browser.wait(
+      ExpectedConditions.textToBePresentInElement($('tmo-root'), 'okreads')
+    );
+    const form = await $('form');
+    const input = await $('input[type="search"]');
+    await input.sendKeys('javascript');
+    await form.submit();
+    browser.sleep(2000);
+
+    await element(by.id('btn-action-1')).click();
+    browser.sleep(2000);
+
+    const readingListToggle = await $('[data-testing="toggle-reading-list"]');
+    await readingListToggle.click();
+
+    await element(by.id('book1')).click();
+    browser.sleep(2000);
+    const snackBar = element(by.tagName('snack-bar-container'));
+    await browser.wait(ExpectedConditions.visibilityOf(snackBar), 30000);
+    const undo = element(by.css('.mat-simple-snackbar-action'));
+    await browser.wait(ExpectedConditions.visibilityOf(undo), 10000);
+    undo.click();
   });
 });
