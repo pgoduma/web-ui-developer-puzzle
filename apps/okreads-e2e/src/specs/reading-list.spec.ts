3656478c6ee5a0a81d4400a4574b172aab43d60e
Pradeep Chandra Goduma <pgoduma@pkrmsezdc.com>
2021-02-05 19:24:33 +0530

    Updated e2e test

--- apps/okreads-e2e/src/specs/reading-list.spec.ts
+++ apps/okreads-e2e/src/specs/reading-list.spec.ts
@@ -1,16 +1,12 @@
 import { $, browser, by, element, ExpectedConditions } from 'protractor';
 
 describe('When: I use the reading list feature', () => {
-  beforeAll(async ()=>{
+  it('Then: I should see my reading list', async () => {
     await browser.get('/');
     await browser.wait(
       ExpectedConditions.textToBePresentInElement($('tmo-root'), 'okreads')
     );
-  })
-  it('Then: I should see my reading list', async () => {
-    const defaultQuery = await $('[data-testing="default-query"]');
-    await defaultQuery.click();
-    await browser.sleep(3000);
+
     const readingListToggle = await $('[data-testing="toggle-reading-list"]');
     await readingListToggle.click();
 
@@ -22,6 +18,22 @@ describe('When: I use the reading list feature', () => {
     );
   });
   it('Then: Should be able to mark book as finished', async () => {
+    await browser.get('/');
+    await browser.wait(
+      ExpectedConditions.textToBePresentInElement($('tmo-root'), 'okreads')
+    );
+    const form = await $('form');
+    const input = await $('input[type="search"]');
+    await input.sendKeys('javascript');
+    await form.submit();
+    browser.sleep(2000);
+
+    await element(by.id('action-btn-1')).click();
+    browser.sleep(2000);
+
+    const readingListToggle = await $('[data-testing="toggle-reading-list"]');
+    await readingListToggle.click();
+
     await element(by.id('action-finished-1')).click();
     const readingListClose = await $('[data-testing="close-reading-list"]');
     await readingListClose.click();
